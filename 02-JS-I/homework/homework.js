337a5a49ccb5a832ea34dd5debedd67ee1076323
Lourdes.Diaz <Lourdes.Diaz@ust-global.com>
2022-03-21 10:50:01 -0400

    valor suma

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  Return (str);
   
 }
 
@@ -35,7 +36,7 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  const sumar = (x+y)
+  var sumar = (x+y)
    return (sumar);
 
 }
